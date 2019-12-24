b930af0c9a9372d411436862d5dd7d3c26eea3bf
DamirMoore <damirmoore94@gmail.com>
2019-12-24 13:42:58 -0600

    started project and added dependencies

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// routes
+app.use("/", require("./routes/index"));
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
